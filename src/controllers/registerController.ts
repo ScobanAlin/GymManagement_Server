0d952e8a47fa96d3d259a07ec96986ade9aba3ef
alin-gabrielscoban <scoban_alyn@yahoo.com>
2026-03-01 17:54:56 +0200

    coach/admin register selector removed

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express';
 import { createUser } from '../models/userModel';
 
 export const register = async (req: Request, res: Response) => {
-    const { first_name, last_name, email, password, role } = req.body;
+    const { first_name, last_name, email, password } = req.body;
 
-    if (!email || !password || !first_name || !last_name || !role) {
-        return res.status(400).json({ message: "All fields (first_name, last_name, email, password, role) are required." });
+    if (!email || !password || !first_name || !last_name) {
+        return res.status(400).json({ message: "All fields (first_name, last_name, email, password) are required." });
     }
 
     try {
+        const role = "coach" as const;
         const newUser = await createUser({ first_name, last_name, email, password, role });
         return res.status(201).json({ message: "User registered successfully.", user: newUser });
     } catch (error: any) {
